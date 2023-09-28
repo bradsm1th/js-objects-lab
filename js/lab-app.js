4cc44b01eccb1cb9f9c669e09b928699c65c11c6
Brad Smith <bradsmith@duck.com>
2023-09-28 12:36:32 -0400

    Add exercise 5

--- js/lab-app.js
+++ js/lab-app.js
@@ -64,7 +64,10 @@ album3.albumDetails.released = new Date(album3.albumDetails['released']);
 
 // Exercise 5:  Add a property named label with the value 'Sire' to album4's albumDetails property
 
+album4.albumDetails.label = "Sire";
 
+// check
+console.log(album4.albumDetails.label);
 
 
 const album5 = {
